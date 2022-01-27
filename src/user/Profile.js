c73da9b14ba993d6a794e1d6c0eef1feaa458f5a
rupesh <rupesh.kumar1733@gmail.com>
2022-01-27 22:21:22 +0530

    unfollow and follows done with front end

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -4,22 +4,47 @@ import { Redirect, Link } from 'react-router-dom';
 import { read } from './apiUser';
 import DefaultProfile from '../images/avatar.jpg';
 import DeleteUser from './DeleteUser';
+import FollowProfileButton from './FollowProfileButton';
 class Profile extends Component {
   constructor() {
     super();
     this.state = {
-      user: '',
+      user: {follow:[],followers:[]},
       redirectToSignin: false,
+      following: false,
+      error: '',
     };
   }
+  //check follow
+  checkFollow = (user) => {
+    const jwt = isAuthenticated();
+    const match = user.followers.find((follower) => {
+      //one id has many others ids(followers )and vice versa
+      return follower._id === jwt.user._id;
+    });
+    return match;
+  };
 
+  clickFollowButton = (callApi) => {
+    const userId = isAuthenticated().user._id;
+    const token = isAuthenticated().token;
+
+    callApi(userId, token, this.state.user._id).then((data) => {
+      if (data.error) {
+        this.setState({ error: data.error });
+      } else {
+        this.setState({ user: data, following: !this.state.following });
+      }
+    });
+  };
   init = (userId) => {
     const token = isAuthenticated().token;
     read(userId, token).then((data) => {
       if (data.error) {
         this.setState({ redirectToSignin: true });
       } else {
-        this.setState({ user: data });
+        let following = this.checkFollow(data);
+        this.setState({ user: data, following });
       }
     });
   };
@@ -38,18 +63,18 @@ class Profile extends Component {
     if (redirectToSignin) {
       return <Redirect to='/signin' />;
     }
-      const photoUrl = user._id
-        ? `${process.env.REACT_APP_API_URL}/user/photo/${
-            user._id
-          }?${new Date().getTime()}`
-        : DefaultProfile;
+    const photoUrl = user._id
+      ? `${process.env.REACT_APP_API_URL}/user/photo/${
+          user._id
+        }?${new Date().getTime()}`
+      : DefaultProfile;
 
     return (
       <div className='container'>
         <h2 className='mt-5 mb-5'>Profile</h2>
 
         <div className='row'>
-          <div className='col-md-6'>
+          <div className='col-md-4'>
             <img
               style={{ height: '200px', width: 'auto' }}
               className='img-thumbnail'
@@ -58,13 +83,14 @@ class Profile extends Component {
               alt={user.name}
             />
           </div>
-          <div className='col-md-6'>
+          <div className='col-md-8'>
             <div className='lead mt-2 '>
               <p> Hello {user.name} </p>
               <p> Email: {user.email} </p>
               <p>{`Joined ${new Date(user.created).toDateString()}`}</p>
             </div>
-            {isAuthenticated().user && isAuthenticated().user._id === user._id && (
+            {isAuthenticated().user &&
+            isAuthenticated().user._id === user._id ? (
               <div className='d-inline-block'>
                 <Link
                   className='btn btn-raised btn-success mr-5'
@@ -74,13 +100,18 @@ class Profile extends Component {
                 </Link>
                 <DeleteUser userId={user._id} />
               </div>
+            ) : (
+              <FollowProfileButton
+                following={this.state.following}
+                onButtonClick={this.clickFollowButton}
+              />
             )}
           </div>
         </div>
-        <div className="row">
-          <div className="col md-12 mt-5 mb-5">
+        <div className='row'>
+          <div className='col md-12 mt-5 mb-5'>
             <hr />
-            <p>{user.about}</p>
+            <p className='lead'>{user.about}</p>
             <hr />
           </div>
         </div>
--- src/user/apiUser.js
+++ src/user/apiUser.js
@@ -63,13 +63,42 @@ export const list = () => {
 };
 
 export const updateUser = (user, next) => {
-  if (typeof window !== 'undefined')
-  {
+  if (typeof window !== 'undefined') {
     if (localStorage.getItem('jwt')) {
-      let auth = JSON.parse(localStorage.getItem('jwt'))
-      auth.user = user
-      localStorage.setItem('jwt', JSON.stringify(auth))
-      next()
+      let auth = JSON.parse(localStorage.getItem('jwt'));
+      auth.user = user;
+      localStorage.setItem('jwt', JSON.stringify(auth));
+      next();
     }
-    }
-}
+  }
+};export const follow = (userId, token, followId) => {
+  return fetch(`${process.env.REACT_APP_API_URL}/user/follow`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ userId, followId }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const unfollow = (userId, token, unfollowId) => {
+  return fetch(`${process.env.REACT_APP_API_URL}/user/unfollow`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ userId, unfollowId }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
