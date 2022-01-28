82ad011ef6a15d64379701986248ce96d5d01257
rupesh <rupesh.kumar1733@gmail.com>
2022-01-28 23:44:20 +0530

    donot know

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -8,7 +8,9 @@ import Profile from './user/Profile';
 import Users from './user/Users';
 import EditProfile from './user/EditProfile';
 import FindPeople from './user/FindPeople';
+import NewPost from './post/NewPost';
 import PrivateRoute from './auth/PrivateRoute';
+
 const MainRouter = () => {
   return (
     <div>
@@ -22,6 +24,7 @@ const MainRouter = () => {
         <PrivateRoute exact path='/user/edit/:userId' component={EditProfile} />
         <PrivateRoute exact path='/findpeople' component={FindPeople} />
         <PrivateRoute exact path='/user/:userId' component={Profile} />
+        <PrivateRoute exact path='/post/create' component={NewPost} />
       </Switch>
     </div>
   );
